c0f3b366110b4c5e61b06a0acbdb3e19aa2b0379
Cyril Mougel <cyril.mougel@gmail.com>
2013-11-20 11:09:06 +0100

    Add Rails engine in plugin to access to his asset

--- lib/errbit_github_plugin.rb
+++ lib/errbit_github_plugin.rb
@@ -1,6 +1,7 @@
 require "errbit_github_plugin/version"
 require 'errbit_github_plugin/error'
 require 'errbit_github_plugin/issue_tracker'
+require 'errbit_github_plugin/rails'
 
 ErrbitPlugin::Register.add_issue_tracker(
   'IssueTrackers::GithubIssuesTracker',
---
+++ lib/errbit_github_plugin/rails.rb
@@ -0,0 +1,8 @@
+if defined?(Rails)
+module ErrbitGithubPlugin
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
+end
