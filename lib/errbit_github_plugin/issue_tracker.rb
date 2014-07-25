c387e73f55ed3324cc11d1f3ed9cc7d9d7f7ba26
Stephen Crosby <stevecrozz@gmail.com>
2014-07-25 15:27:43 -0700

    initial import

--- lib/errbit_github_plugin/issue_tracker.rb
+++ lib/errbit_github_plugin/issue_tracker.rb
@@ -3,22 +3,41 @@ require 'octokit'
 module ErrbitGithubPlugin
   class IssueTracker < ErrbitPlugin::IssueTracker
 
-    def label; 'github'; end
-    def note
-      'Please configure your github repository in the <strong>GITHUB REPO</strong> field above.<br/>' <<
-      'Instead of providing your username & password, you can link your Github account ' <<
-      'to your user profile, and allow Errbit to create issues using your OAuth token.'
+    LABEL = 'github'
+
+    NOTE = 'Please configure your github repository in the <strong>GITHUB ' <<
+           'REPO</strong> field above.<br/> Instead of providing your ' <<
+           'username & password, you can link your Github account to your ' <<
+           'user profile, and allow Errbit to create issues using your ' <<
+           'OAuth token.'
+
+    FIELDS = {
+      :username => {
+        :placeholder => "Your username on GitHub"
+      },
+      :password => {
+        :placeholder => "Password for your account"
+      }
+    }
+
+    def self.label
+      LABEL
     end
 
-    def fields
-      {
-        :username => {
-          :placeholder => "Your username on GitHub"
-        },
-        :password => {
-          :placeholder => "Password for your account"
-        }
-      }
+    def self.note
+      NOTE
+    end
+
+    def self.fields
+      FIELDS
+    end
+
+    def self.body_template
+      @body_template ||= ERB.new(File.read(
+        File.join(
+          ErrbitGithubPlugin.root, 'views', 'github_issues_body.txt.erb'
+        )
+      ))
     end
 
     attr_accessor :oauth_token
@@ -35,9 +54,11 @@ module ErrbitGithubPlugin
     end
 
     def check_params
-      if Fields.detect {|f| self[f[0]].blank? }
-        errors.add :base, 'You must specify your GitHub username and password'
+      errors = []
+      if self.class.fields.detect {|f| params[f[0]].blank? }
+        errors << [:base, 'You must specify your GitHub username and password']
       end
+      errors
     end
 
     def create_issue(problem, reported_by = nil)
@@ -52,21 +73,17 @@ module ErrbitGithubPlugin
         issue = client.create_issue(
           project_id,
           issue_title(problem),
-          body_template.result(binding).unpack('C*').pack('U*')
+          self.class.body_template.result(binding).unpack('C*').pack('U*')
         )
         @url = issue.html_url
         problem.update_attributes(
           :issue_link => issue.html_url,
-          :issue_type => Label
+          :issue_type => self.class.label
         )
 
       rescue Octokit::Unauthorized
         raise ErrbitGithubPlugin::AuthenticationError, "Could not authenticate with GitHub. Please check your username and password."
       end
-    end
-
-    def body_template
-      @@body_template ||= ERB.new(File.read(Rails.root + "app/views/issue_trackers/github_issues_body.txt.erb").gsub(/^\s*/, ''))
     end
   end
 end
