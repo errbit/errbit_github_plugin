143ece838f5d4740a316967bff94af2d787cbd0b
Stephen Crosby <stevecrozz@gmail.com>
2015-03-15 17:45:30 -0700

    new tracker icon interface

--- lib/errbit_github_plugin.rb
+++ lib/errbit_github_plugin.rb
@@ -1,11 +1,14 @@
 require "errbit_github_plugin/version"
 require 'errbit_github_plugin/error'
 require 'errbit_github_plugin/issue_tracker'
-require 'errbit_github_plugin/rails'
 
 module ErrbitGithubPlugin
   def self.root
     File.expand_path '../..', __FILE__
+  end
+
+  def self.read_static_file(file)
+    File.read(File.join(self.root, 'static', file))
   end
 end
 
