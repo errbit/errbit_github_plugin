1ad651489cd6907c8ec449eb3b54d38c16d20cfa
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-23 14:33:47 +0100

    Remove dead code

--- lib/errbit_github_plugin.rb
+++ lib/errbit_github_plugin.rb
@@ -4,13 +4,6 @@ require "errbit_github_plugin/error"
 require "errbit_github_plugin/issue_tracker"
 
 module ErrbitGithubPlugin
-  def self.root
-    File.expand_path '../..', __FILE__
-  end
-
-  def self.read_static_file(file)
-    File.read(File.join(self.root, 'static', file))
-  end
 end
 
 ErrbitPlugin::Registry.add_issue_tracker(ErrbitGithubPlugin::IssueTracker)
