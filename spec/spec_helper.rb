39d7b07c6f04aa0204f16b46244a00743d566973
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-10 21:58:46 +0200

    Remove coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,5 @@
 if ENV['COVERAGE']
   require 'simplecov'
-  if ENV['CI']
-    require 'coveralls'
-    Coveralls.wear!
-    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
-    ]
-  end
 
   SimpleCov.start
 end
