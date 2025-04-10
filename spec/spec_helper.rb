5716f0dd867bc0ac1c3d3bd9b39a629ed1de4d27
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-10 23:10:36 +0200

    Configure RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,12 +13,13 @@ require "errbit_github_plugin"
 require "active_support/all"
 
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
